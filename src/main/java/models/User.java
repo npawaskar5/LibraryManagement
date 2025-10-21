dfae74c6b93eb94028d4a3948b0c28fc6b40ff7f
Nihaal-Ahmed <npawaskar5@gmail.com>
2025-10-21 20:01:10 +0530

    add User class

---
+++ src/main/java/models/User.java
@@ -0,0 +1,27 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String name;
+    private List<Book> borrowedBooks;
+
+    public User(String name) {
+        this.name = name;
+        borrowedBooks = new ArrayList<>();
+    }
+
+    public String getName() { return name; }
+    public List<Book> getBorrowedBooks() { return borrowedBooks; }
+
+    public void borrowBook(Book book) {
+        borrowedBooks.add(book);
+        book.borrow();
+    }
+
+    public void returnBook(Book book) {
+        borrowedBooks.remove(book);
+        book.returned();
+    }
+}
