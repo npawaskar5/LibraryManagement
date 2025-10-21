eb6e5139d4790967dfecbac067b7f7d0488ac4aa
Nihaal-Ahmed <npawaskar5@gmail.com>
2025-10-21 20:00:28 +0530

    add Book class

---
+++ src/main/java/models/Book.java
@@ -0,0 +1,25 @@
+package models;
+
+public class Book {
+    private String title;
+    private String author;
+    private boolean isBorrowed;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isBorrowed = false;
+    }
+
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public boolean isBorrowed() { return isBorrowed; }
+
+    public void borrow() { isBorrowed = true; }
+    public void returned() { isBorrowed = false; }
+
+    @Override
+    public String toString() {
+        return title + " by " + author + (isBorrowed ? " (Borrowed)" : "");
+    }
+}
